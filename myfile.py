0cd5531a43eea17fa81e55fe0529cef8d04d18b1
ponnadarudra99-wq <ponnadarudra99@gmail.com>
2025-12-25 16:48:26 +0530

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -1,1 +1,15 @@
+import pickle
+import streamlit as st
 
+model=pickle.load(open("area.pkl","rb"))
+
+def myf1():
+    st.title("area price prediction")
+    area=st.number_input("enter the area value...")
+    pred=st.button("predict")
+
+    if pred:
+       op=model.predict([[area]])
+       st.write("price of the area : ",op[0])
+ 
+myf1()
